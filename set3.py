122097b267f5c54bd20de074dcfd5e58bae7ec9d
kaviya-s <41141529+kaviya-s@users.noreply.github.com>
2018-07-12 13:10:35 +0530

    Update set3.py

--- set3.py
+++ set3.py
@@ -1,5 +1,7 @@
 n=input()
 if n=='a' or n=='e' or n=='i' or n=='o' or n=='u':
 	print("Vowel")
+elif n=='$' or n=='@' or n=='!' or n=='#' or n=='%' or n=='&':
+	print("invalid")
 else:
 	print("Consonant")
